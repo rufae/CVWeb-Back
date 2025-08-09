7c27db57c5cfc0317f3b042dd07aa382a5e9c9d8
Rufae <rafaelcastanoblanca1805@gmail.com>
2025-08-09 12:20:52 +0200

    Corrección CORS

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://cv-web-kappa.vercel.app/",
+    "https://cv-web-kappa.vercel.app",
 ]
 
 app.add_middleware(
